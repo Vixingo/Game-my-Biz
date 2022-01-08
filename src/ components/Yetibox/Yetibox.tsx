11a9096e4b0a1e3519bb18fbbab76bb185033e88
Md. Nazmul Khandoker <vixingo@Mds-MacBook-Pro.local>
2022-01-08 16:12:38 +0600

    yetigamebox

--- src/ components/Yetibox/Yetibox.tsx
+++ src/ components/Yetibox/Yetibox.tsx
@@ -1,13 +1,56 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import { Box, Stack, Theme, Typography } from "@mui/material";
+import { ResponsiveStyleValue } from "@mui/system/styleFunctionSx";
+import { Property } from "csstype";
+import React, { useEffect } from "react";
 
-function Yetibox() {
+// let colorss: string;
+
+function Yetibox(props: {
+    title: string;
+    amount: string | number;
+    percent: number;
+    imgsrc: string;
+}): JSX.Element {
+    // useEffect(() => {
+    //     colorss = props.percent > 0 ? "#21A300" : "#FF0000";
+    //     console.log(colorss);
+    // });
     return (
         <>
-            <Box sx={{ maxWidth: "200px" }}>
-                <Typography>Points</Typography>
-                <Typography>1,394</Typography>
-                <Typography>+40%</Typography>
+            <Box
+                sx={{
+                    margin: "10px 5px",
+                    width: "200px",
+                    backgroundColor: "#fff",
+                    boxShadow: "0px 18px 36px 2px rgba(135, 135, 135, 0.1);",
+                    borderRadius: "37px",
+                    padding: "20px ",
+                }}
+            >
+                <Stack direction={"row"} justifyContent={"space-between"}>
+                    <Typography variant="h5" color={"#656565"} fontWeight={500}>
+                        {props.title}
+                    </Typography>
+                    <img
+                        src={props.imgsrc}
+                        alt=""
+                        width="35px"
+                        style={{
+                            boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.16)",
+                        }}
+                    />
+                </Stack>
+                <Typography
+                    variant="h3"
+                    fontWeight={"bold"}
+                    color={"#000"}
+                    mt={2}
+                >
+                    {props.amount}
+                </Typography>
+                <Typography variant="h6" color={"#21A300"} textAlign={"right"}>
+                    {props.percent}%
+                </Typography>
             </Box>
         </>
     );
--- src/ components/Yetiinfo/Yetiinfo.tsx
+++ src/ components/Yetiinfo/Yetiinfo.tsx
@@ -1,6 +1,38 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import React from "react";
 import Yetibox from "../Yetibox/Yetibox";
+const Yetiboxdata = [
+    {
+        title: "Points",
+        amount: "1,394",
+        percent: +40,
+        imgsrc: "../img/points.png",
+    },
+    {
+        title: "Revenue",
+        amount: "$8,696",
+        percent: +50,
+        imgsrc: "../img/rev.png",
+    },
+    {
+        title: "Reviews",
+        amount: "9",
+        percent: +3,
+        imgsrc: "../img/review.png",
+    },
+    {
+        title: "Upsells",
+        amount: "$525",
+        percent: +5,
+        imgsrc: "../img/upsell.png",
+    },
+    {
+        title: "Sales",
+        amount: "3,642",
+        percent: -2,
+        imgsrc: "../img/sales.png",
+    },
+];
 function Yetiinfo() {
     return (
         <>
@@ -20,8 +52,23 @@ function Yetiinfo() {
                             />
                         </Box>
                     </div>
-                    <div className="col-lg-6">
-                        <Yetibox />
+                    <div className="col-lg-6  ">
+                        <Stack
+                            direction={"row"}
+                            flexWrap={"wrap"}
+                            justifyContent={"center"}
+                        >
+                            {Yetiboxdata.map((val, index) => {
+                                return (
+                                    <Yetibox
+                                        title={val.title}
+                                        amount={val.amount}
+                                        percent={val.percent}
+                                        imgsrc={val.imgsrc}
+                                    />
+                                );
+                            })}
+                        </Stack>
                     </div>
                 </div>
             </div>
