bc3a3dee20db3117e9fb21a5f405a55c5510a68d
Md. Nazmul Khandoker <vixingo@Mds-MacBook-Pro.local>
2022-01-10 22:06:57 +0600

    leader

--- src/ components/LeaderBoard/LeaderBoard.tsx
+++ src/ components/LeaderBoard/LeaderBoard.tsx
@@ -1,22 +1,93 @@
 import { Box } from "@mui/material";
-import React from "react";
-import Tab from "@mui/material/Tab";
-import TabContext from "@mui/lab/TabContext";
-import TabList from "@mui/lab/TabList";
-import TabPanel from "@mui/lab/TabPanel";
+import React, { useState } from "react";
+import "./leader.css";
+import Leaders from "./Leaders";
 
 function LeaderBoard(): JSX.Element {
+    const [toggleState, setToggleState] = useState(2);
+
+    const toggleTab = (index: number) => {
+        setToggleState(index);
+    };
     return (
         <>
             <Box
                 className="container"
                 sx={{
-                    py: 3,
+                    py: 4,
+                    px: 8,
                     backgroundImage: "url(../img/inter.png)",
                     backgroundRepeat: "no-repeat",
                     backgroundSize: "100%",
                 }}
-            ></Box>
+            >
+                <section className="Home_tabs">
+                    <div
+                        className="tab_btn_holder "
+                        style={{ textAlign: "right" }}
+                    >
+                        <button
+                            className={
+                                toggleState === 1 ? "tabs active_tabs" : "tabs"
+                            }
+                            onClick={() => toggleTab(1)}
+                        >
+                            Today{" "}
+                        </button>
+                        <button
+                            className={
+                                toggleState === 2 ? "tabs active_tabs" : "tabs"
+                            }
+                            onClick={() => toggleTab(2)}
+                        >
+                            Week{" "}
+                        </button>
+                        <button
+                            className={
+                                toggleState === 3 ? "tabs active_tabs" : "tabs"
+                            }
+                            onClick={() => toggleTab(3)}
+                        >
+                            Month{" "}
+                        </button>
+                    </div>
+
+                    <div className="tab_content_holder">
+                        <div
+                            className={
+                                toggleState === 1
+                                    ? "content  active-content"
+                                    : "content"
+                            }
+                        >
+                            <h1 style={{ color: "#fff", padding: "30px" }}>
+                                There is nothing here
+                            </h1>
+                        </div>
+
+                        <div
+                            className={
+                                toggleState === 2
+                                    ? "content  active-content"
+                                    : "content"
+                            }
+                        >
+                            <Leaders />{" "}
+                        </div>
+                        <div
+                            className={
+                                toggleState === 3
+                                    ? "content  active-content"
+                                    : "content"
+                            }
+                        >
+                            <h1 style={{ color: "#fff", padding: "30px" }}>
+                                There is nothing here
+                            </h1>
+                        </div>
+                    </div>
+                </section>
+            </Box>
         </>
     );
 }
---
+++ src/ components/LeaderBoard/Leaders.tsx
@@ -0,0 +1,127 @@
+import { Box, Stack, Typography } from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import React from "react";
+
+function Leaders() {
+    return (
+        <>
+            <section className="Leaders">
+                <div className="container">
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexWrap: "wrap",
+                            justifyContent: "space-evenly",
+                            color: "#FFF",
+                            my: "40px",
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography variant="h4">2</Typography>
+                            <ArrowDropDownIcon fontSize="large" />
+                            <img
+                                src="../img/P_2.png"
+                                alt=""
+                                width={"150px"}
+                                style={{
+                                    border: "2px solid #EE238A",
+                                    boxShadow: "0px 18px 36px 5px #00000040",
+                                    borderRadius: "50%",
+                                    margin: "10px ",
+                                }}
+                            />
+                            <Typography fontSize={"large"}>
+                                Courtney Henry
+                            </Typography>
+                            <Typography
+                                fontSize={"large"}
+                                sx={{ color: "#EE238A" }}
+                            >
+                                8032
+                            </Typography>
+                        </Box>
+                        {/* item break */}
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                position: "relative",
+                                top: "-50px",
+                            }}
+                        >
+                            <Typography variant="h4">1</Typography>
+                            <img src="../img/king.png" alt="" width={"50%"} />
+
+                            {/* <ArrowDropDownIcon fontSize="large" /> */}
+                            <img
+                                src="../img/P_1.png"
+                                alt=""
+                                width={"150px"}
+                                style={{
+                                    border: "5px solid #EE238A",
+                                    boxShadow:
+                                        "0px 0px 32px 6px rgba(238, 35, 138, 0.9)",
+                                    borderRadius: "50%",
+                                    margin: "10px ",
+                                }}
+                            />
+                            <Typography fontSize={"large"}>
+                                Wade Warren
+                            </Typography>
+                            <Typography
+                                fontSize={"large"}
+                                sx={{ color: "#EE238A" }}
+                            >
+                                8122
+                            </Typography>
+                        </Box>
+                        {/* item break */}
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography variant="h4">3</Typography>
+                            <ArrowDropUpIcon fontSize="large" color="primary" />
+                            <img
+                                src="../img/P_3.png"
+                                alt=""
+                                width={"150px"}
+                                style={{
+                                    border: "2px solid #EE238A",
+                                    boxShadow: "0px 18px 36px 5px #00000040",
+                                    borderRadius: "50%",
+                                    margin: "10px ",
+                                }}
+                            />
+                            <Typography fontSize={"large"}>
+                                Munzira Khan{" "}
+                            </Typography>
+                            <Typography
+                                fontSize={"large"}
+                                sx={{ color: "#EE238A" }}
+                            >
+                                7884
+                            </Typography>
+                        </Box>
+                    </Box>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Leaders;
